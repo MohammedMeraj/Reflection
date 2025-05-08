27f2974ec76e19aa96c52ee6df3534b62be325ac
MohammedMeraj <mdmomin7517@gmail.com>
2025-05-09 02:23:24 +0530

    Test redirect funtionality

---
+++ app/test/page.tsx
@@ -0,0 +1,14 @@
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+import React from 'react'
+
+const page = () => {
+  return (
+    <div>
+       <Link href="https://wa.me/1111111111?text=sMPLWWED"> <Button>send</Button></Link>
+
+    </div>
+  )
+}
+
+export default page
